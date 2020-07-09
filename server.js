db3286a4b8d5ab4d5e6f44faec74dcd26762486f
Scott Griffin <mscottgriffin@gmail.com>
2020-07-09 10:45:44 -0400

    added express use static, app getapi.

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // view / html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 /// api / json
+app.get('/api/notes', (req, res) => {
+  res.json(db);
+});
 
 app.listen(PORT, (req, res) => {
   console.log('currently running on http://localhost:${PORT}');
