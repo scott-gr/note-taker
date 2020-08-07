e081da70084f8fd18d247ec1dafedc6182c42785
Scott Griffin <mscottgriffin@gmail.com>
2020-08-07 01:23:21 -0400

    new assets folder for note wrting function and css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { fstat } = require('fs');
-const { listenerCount } = require('process');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,18 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-// /// api / json
-// app.get('/api/notes', (req, res) => {
-//   fs.readFile('./db/db.json', 'utf-8', (err,data) => {
-//     if (err) {
-//       console.log('Sorry! We could not read your note.');
-//     }
-//     db = JSON.parse(data);
-//     res.json(db);
-//   });
-// });
-
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 
 
