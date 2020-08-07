9782f444ae281a0736fbc45dcd797683255b0705
Scott Griffin <mscottgriffin@gmail.com>
2020-08-07 01:07:37 -0400

    moving routes from server.js to routes folder in root.

--- server.js
+++ server.js
@@ -9,23 +9,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// view / html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-/// api / json
-app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf-8', (err,data) => {
-    if (err) {
-      console.log('Sorry! We could not read your note.');
-    }
-    db = JSON.parse(data);
-    res.json(db);
-  });
-});
+
+// /// api / json
+// app.get('/api/notes', (req, res) => {
+//   fs.readFile('./db/db.json', 'utf-8', (err,data) => {
+//     if (err) {
+//       console.log('Sorry! We could not read your note.');
+//     }
+//     db = JSON.parse(data);
+//     res.json(db);
+//   });
+// });
 
 
 
