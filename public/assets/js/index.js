697e0c6d61564d51faddb815da0a2a0f1bebc27c
Scott Griffin <mscottgriffin@gmail.com>
2020-08-07 05:58:51 -0400

    get, post, delete functions

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,31 @@
-const { text } = require("express");
+
 
 //title, text, save btn, new btn, list
+
+//track note that is currently being added
+let currentNote = {}
+
+//get notes from db
+const getNotes = () => {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET",
+  });
+};
+
+//save note to db
+const postNote = (note) => {
+  return $.ajax({
+    url: "/api/notes",
+    data: note,
+    method: "POST",
+  });
+};
+
+//delete note from db
+const deleteNote = (id) => {
+  return $.ajax({
+    url: "api/notes/" + id,
+    method: "DELETE",
+  });
+};
